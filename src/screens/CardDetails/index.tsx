db50d8be89f87311434cce0d8769429dcd9d3e5a
Gabriel Martins <gsmartins96@hotmail.com>
2021-12-16 11:01:38 -0300

    add some details about the car in cardetails screen

--- src/screens/CardDetails/index.tsx
+++ src/screens/CardDetails/index.tsx
@@ -3,6 +3,15 @@ import {
   Container,
   Header,
   CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About
 } from './styles'
 
 import { BackButton } from '../../components/BackButton'
@@ -20,6 +29,26 @@ export function CardDetails(){
           imagesUrl={['https://static.wikia.nocookie.net/forzamotorsport/images/1/12/HOR_XB1_BMW_M4_14.png/revision/latest?cb=20191111201655']} 
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>BMW</Brand>
+            <Name>M4 CS</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 320</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este automóvel é meu sonho de consumo mais alcançavel nos proximo 5~10 anos
+          dependendo somente do quanto eu sou bom no que faço e no quanto estou disposto
+          a se dedicar para realizar meus sonhos.
+        </About>
+      </Content>
     </Container>
   )
 }
