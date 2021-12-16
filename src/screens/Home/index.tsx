fde9820b1152295fc8c6bfdff83978854ec66d24
Gabriel Martins <gsmartins96@hotmail.com>
2021-12-16 09:51:33 -0300

    ✨ Create car card component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -9,8 +9,29 @@ import {
 } from './styles'
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 export function Home(){
+  const carDataOne = {
+    brand: 'bmw',
+    name: 'M4 CS',
+    rent: {
+      period: 'Ao dia',
+      price: 320
+    },
+    thumbnail: 'https://static.wikia.nocookie.net/forzamotorsport/images/1/12/HOR_XB1_BMW_M4_14.png/revision/latest?cb=20191111201655'
+  }
+
+  const carDataTwo = {
+    brand: 'VW',
+    name: 'Golf GTI',
+    rent: {
+      period: 'Ao dia',
+      price: 130
+    },
+    thumbnail: 'https://assets-global.website-files.com/5c4b6af93b460b2d2ec8e8e6/602d23cacb385ebfc20ad889_consorcio-golf-gti.png'
+  }
+
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
@@ -26,6 +47,9 @@ export function Home(){
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   )
 }
