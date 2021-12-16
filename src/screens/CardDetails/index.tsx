0b77cc8bfe4176b7bfe3954588c48e9d70d7a727
Gabriel Martins <gsmartins96@hotmail.com>
2021-12-16 10:48:01 -0300

    New details in CarSlider image and CardDetails screen

--- src/screens/CardDetails/index.tsx
+++ src/screens/CardDetails/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {
   Container,
-  Header
+  Header,
+  CarImages,
 } from './styles'
 
 import { BackButton } from '../../components/BackButton'
+import { ImageSlider } from '../../components/ImageSlider'
 
 export function CardDetails(){
   return (
@@ -12,6 +14,12 @@ export function CardDetails(){
       <Header>
         <BackButton onPress={() => {}}/>
       </Header>
+
+      <CarImages>
+        <ImageSlider 
+          imagesUrl={['https://static.wikia.nocookie.net/forzamotorsport/images/1/12/HOR_XB1_BMW_M4_14.png/revision/latest?cb=20191111201655']} 
+        />
+      </CarImages>
     </Container>
   )
 }
