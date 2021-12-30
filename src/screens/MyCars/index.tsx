887c24905d71e6bb5d6d592c08c91f31bd06de2a
Gabriel Martins <gsmartins96@hotmail.com>
2021-12-30 12:47:12 -0300

    Exibindo carros agendados e estilização de tela

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,13 +1,41 @@
 import React, { useEffect, useState } from 'react'
+import { StatusBar, FlatList } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'
 import {
-  Container
+  Container,
+  Header, 
+  SubTitle, 
+  Title,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarWrapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterPeriod,
+  CarFooterDate,
 } from './styles'
+import theme from '../../styles/theme';
 
-import { CarDTO } from '../../dtos/CarsDTO'
 import api from '../../services/api';
 
+import { Car } from '../../components/Car'
+import { BackButton } from '../../components/BackButton';
+
+import { CarDTO } from '../../dtos/CarsDTO'
+import { Load } from '../../components/Load';
+
+interface CarProps{
+  id: string;
+  user_id: string;
+  car: CarDTO;
+  startDate: string;
+  endDate: string;
+}
+
 export function MyCars(){
-  const [cars, setCars] = useState<CarDTO[]>([])
+  const [cars, setCars] = useState<CarProps[]>([])
   const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
@@ -28,7 +56,45 @@ export function MyCars(){
 
   return (
     <Container>
-    
+      <StatusBar barStyle="light-content" translucent backgroundColor="transparent"/>
+      <Header>
+        <BackButton color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma {'\n'}data de início e {'\n'}fim do aluguel 
+        </Title>
+
+        <SubTitle>Confoto, segurança e praticidade</SubTitle>
+      </Header>
+
+      {loading ? <Load /> : 
+        <Content>
+          <Appointments>
+            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+            <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+          </Appointments>
+
+          <FlatList
+            data={cars}
+            keyExtractor={item => item.id}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <CarWrapper>
+                <Car data={item.car} />  
+                <CarFooter>
+                  <CarFooterTitle>Período</CarFooterTitle>
+                  <CarFooterPeriod>
+                    <CarFooterDate>{item.startDate}</CarFooterDate>
+                    <AntDesign name="arrowright" size={20} color={theme.colors.title} style={{ marginHorizontal: 10 }} />
+                    <CarFooterDate>{item.endDate}</CarFooterDate>
+                  </CarFooterPeriod>
+                </CarFooter>
+              </CarWrapper>
+            )}
+          />
+        </Content>
+      }
+
     </Container>
   )
 }
