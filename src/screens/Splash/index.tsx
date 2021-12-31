2332080084855cc3938acac18d902b8856d1c0b4
Gabriel Martins <gsmartins96@hotmail.com>
2021-12-30 21:04:20 -0300

    fix babel config and splash screen created

---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StatusBar } from 'react-native'
+import {
+  Container
+} from './styles'
+
+export function Splash(){
+  return (
+    <Container>
+      <StatusBar barStyle={'light-content'} />
+      
+    </Container>
+  )
+}
