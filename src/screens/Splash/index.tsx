f335d8d2d6816c909db8d32ddddd844fe5d666b4
Gabriel Martins <gsmartins96@hotmail.com>
2022-01-04 19:52:16 -0300

    Primeira animação e configuração da Splash

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,14 +1,43 @@
 import React from 'react'
-import { StatusBar } from 'react-native'
+import { Button, StyleSheet, Dimensions } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated';
+
 import {
   Container
 } from './styles'
 
+const WIDTH = Dimensions.get('window').width;
+
 export function Splash(){
+  const animation = useSharedValue(0);
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ 
+        translateX:  withTiming(animation.value, {
+          duration: 500,
+          easing: Easing.bezier(0,.66,.97,-0.77)
+        }) 
+      }]
+    }    
+  })
+
+  function handleAnimationsPosition(){
+    animation.value = Math.random() * (WIDTH - 100);
+  }
+
   return (
     <Container>
-      <StatusBar barStyle={'light-content'} />
-      
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button title="Mover" onPress={handleAnimationsPosition} />
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  }
+})
