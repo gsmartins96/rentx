1f28c8feb3a61def1494679e548369138c1a7462
Gabriel Martins <gsmartins96@hotmail.com>
2022-01-05 19:29:26 -0300

    Deixando o botão flutuante com gestos do usuário

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react'
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet, BackHandler } from 'react-native';
+import { PanGestureHandler, RectButton } from 'react-native-gesture-handler';
+import { RFValue } from 'react-native-responsive-fontsize';
 import { useNavigation } from '@react-navigation/native';
 import { useTheme } from 'styled-components';
-import { Ionicons } from '@expo/vector-icons'
-import { RFValue } from 'react-native-responsive-fontsize'
+import { Ionicons } from '@expo/vector-icons';
+import Animated, { 
+  useAnimatedStyle, 
+  useSharedValue, 
+  useAnimatedGestureHandler, 
+  withSpring 
+} from 'react-native-reanimated'
+
 import {
   Container,
   Header,
   HeaderContent,
   TotalCars,
   CarList,
-  MyCarsButton
 } from './styles'
 
 import { Car } from '../../components/Car';
@@ -19,12 +26,40 @@ import Logo from '../../assets/logo.svg';
 
 import api from '../../services/api';
 import { CarDTO } from '../../dtos/CarsDTO';
+import theme from '../../styles/theme';
+
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton)
 
 export function Home(){
+  const theme = useTheme();
   const navigation = useNavigation<any>();
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
-  const theme = useTheme();
+
+  const positionX = useSharedValue(0);
+  const positionY = useSharedValue(0);
+  const myCarsButtonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: positionX.value },
+        { translateY: positionY.value },
+      ]
+    }
+  });
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, ctx: any){
+      ctx.positionX = positionX.value;
+      ctx.positionY = positionY.value;
+    },
+    onActive(event, ctx: any){ // Soma a posição inicial com a nova pra manter e nao voltar pro inicio
+      positionX.value = ctx.positionX + event.translationX;
+      positionY.value = ctx.positionY + event.translationY;
+    },
+    onEnd(){
+      positionX.value = withSpring(0);
+      positionY.value = withSpring(0);
+    }
+  })
 
   function handleNavigateCarDetails(car: CarDTO){
     navigation.navigate('CarDetails', {car});
@@ -49,6 +84,12 @@ export function Home(){
     fetchCars()
   }, [])
 
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      return true
+    })
+  }, [])
+
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
@@ -59,9 +100,11 @@ export function Home(){
             height={RFValue(12)}
           />
 
-          <TotalCars>
-            Total de {cars.length} carros
-          </TotalCars>
+          {!loading && 
+            <TotalCars>
+              Total de {cars.length} carros
+            </TotalCars>
+          }
         </HeaderContent>
       </Header>
 
@@ -76,9 +119,36 @@ export function Home(){
           }
         />
       }
-      <MyCarsButton onPress={handleOpenMyCars}>
-        <Ionicons name='ios-car-sport' size={32} color={theme.colors.shape} />
-      </MyCarsButton>
+
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            myCarsButtonStyle,
+            {
+              position: 'absolute',
+              bottom: 13,
+              right: 22,
+            }
+          ]}
+        >
+          <ButtonAnimated onPress={handleOpenMyCars} style={styles.button}>
+            <Ionicons name='ios-car-sport' size={32} color={theme.colors.shape} />
+          </ButtonAnimated>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    backgroundColor: theme.colors.main,
+  }
+})
