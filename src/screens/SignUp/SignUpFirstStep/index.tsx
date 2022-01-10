671991fe74d44cc00ad5bb02cc7f65783b612414
Gabriel Martins <gsmartins96@hotmail.com>
2022-01-10 20:47:20 -0300

    Adicionado os inputs e configuração de teclado

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import {
   Container,
@@ -12,31 +13,54 @@ import {
 
 import { BackButton } from '../../../components/BackButton'
 import { Bullet } from '../../../components/Bullet'
+import { Input } from '../../../components/Input'
+import { Button } from '../../../components/Button'
 
 export function SignUpFirstStep(){
   const navigation = useNavigation<any>();
   
   return (
-    <Container>
-      <Header>
-        <BackButton />
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
+    <KeyboardAvoidingView behavior='position' enabled >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
 
-      <Title>
-        Crie sua {'\n'}conta
-      </Title>
-      <SubTitle>
-        Faça seu cadastro de {'\n'}
-        forma rápida e fácil
-      </SubTitle>
+          <Title>
+            Crie sua {'\n'}conta
+          </Title>
+          <SubTitle>
+            Faça seu cadastro de {'\n'}
+            forma rápida e fácil
+          </SubTitle>
 
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input 
+              iconName="user"
+              placeholder='Nome'
+            />
+            <Input 
+              iconName="mail"
+              placeholder='E-mail'
+            />
+            <Input 
+              iconName="credit-card"
+              placeholder='CNH'
+            />
+
+          <Button 
+            title="Próximo"
+          />
+
+          </Form>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   )
 }
