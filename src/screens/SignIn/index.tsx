5752a1996ce99a3fac59c4d46a3ce81b0c4e9049
Gabriel Martins <gsmartins96@hotmail.com>
2022-01-10 15:40:55 -0300

    Esconde o teclado quando clica na tela

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { StatusBar, KeyboardAvoidingView } from 'react-native'
+import { 
+  StatusBar, 
+  KeyboardAvoidingView, 
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native'
 import {
   Container,
   Header,
@@ -18,49 +23,51 @@ import theme from '../../styles/theme'
 export function SignIn(){
   return (
     <KeyboardAvoidingView behavior='position' enabled >
-      <Container>
-        <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
-        <Header>
-          <Title>Estamos {'\n'}quase lá.</Title>
-          <SubTitle>
-            Faça seu login para começar {'\n'}
-            uma experiência incrível
-          </SubTitle>
-        </Header>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
+          <Header>
+            <Title>Estamos {'\n'}quase lá.</Title>
+            <SubTitle>
+              Faça seu login para começar {'\n'}
+              uma experiência incrível
+            </SubTitle>
+          </Header>
 
-        <Form>
-          <Input 
-            iconName='mail'
-            placeholder='E-mail'
-            keyboardType='email-address'
-            autoCorrect={false}
-            autoCapitalize='none'
-          />
-          <InputPassword 
-            iconName='lock'
-            placeholder='Senha'
-          />
-          
-        </Form>
+          <Form>
+            <Input 
+              iconName='mail'
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCorrect={false}
+              autoCapitalize='none'
+            />
+            <InputPassword 
+              iconName='lock'
+              placeholder='Senha'
+            />
+            
+          </Form>
 
 
-        <Footer>
-          <Button 
-            title="Login"
-            onPress={() => {}}
-            enabled={true}
-            loading={false}
-          />
-          <Button 
-            title="Criar conta gratuita"
-            color={theme.colors.background_secundary}
-            light
-            onPress={() => {}}
-            enabled={true}
-            loading={false}
-          />
-        </Footer>
-      </Container>
+          <Footer>
+            <Button 
+              title="Login"
+              onPress={() => {}}
+              enabled={true}
+              loading={false}
+            />
+            <Button 
+              title="Criar conta gratuita"
+              color={theme.colors.background_secundary}
+              light
+              onPress={() => {}}
+              enabled={true}
+              loading={false}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   )
 }
