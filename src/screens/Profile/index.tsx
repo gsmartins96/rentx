3d3e58cb538dc7a535d92e0c08a450fbcb776102
Gabriel Martins <gsmartins96@hotmail.com>
2022-01-19 10:40:17 -0300

    adicionado opção de dados e trocar senha

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BackButton } from '../../components/BackButton'
 import { Feather } from '@expo/vector-icons'
 import {
@@ -10,12 +10,22 @@ import {
   PhotoContainer,
   Photo,
   PhotoButton,
+  Content,
+  Options,
+  Option,
+  OptionTitle,
 } from './styles'
 
 import theme from '../../styles/theme'
 
 export function Profile(){
+  const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
+
   function handleSignOut(){}
+
+  function handleOptionChange(optionSelected: 'dataEdit' | 'passwordEdit'){
+    setOption(optionSelected)
+  }
 
   return (
     <Container>
@@ -35,6 +45,27 @@ export function Profile(){
           </PhotoButton>
         </PhotoContainer>
       </Header>
+
+      <Content>
+        <Options>
+          <Option 
+            active={option === 'dataEdit'}
+            onPress={() => handleOptionChange('dataEdit')}
+          >
+            <OptionTitle active={option === 'dataEdit'} >
+              Dados
+            </OptionTitle>
+          </Option>
+          <Option 
+            active={option === 'passwordEdit'}
+            onPress={() => handleOptionChange('passwordEdit')}
+          >
+            <OptionTitle active={option === 'passwordEdit'} >
+              Trocar Senha
+            </OptionTitle>
+          </Option>
+        </Options>
+      </Content>
     </Container>
   )
 }
