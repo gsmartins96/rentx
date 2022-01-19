ead2b22cfbca482862719edafa4e40ff6f704154
Gabriel Martins <gsmartins96@hotmail.com>
2022-01-19 12:33:12 -0300

    adicionado tabela de carro

--- src/database/schema/index.ts
+++ src/database/schema/index.ts
@@ -1,6 +1,7 @@
 import { appSchema } from '@nozbe/watermelondb'
 import { userSchema } from './userSchema'
+import { carSchema } from './carSchema'
 
-const schemas = appSchema({ version: 1, tables: [userSchema] })
+const schemas = appSchema({ version: 2, tables: [userSchema, carSchema] })
 
 export { schemas }
