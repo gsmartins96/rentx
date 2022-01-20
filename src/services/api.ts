6b3808f4eb6a3195d619a8d26b76cfb3e069273a
Gabriel Martins <gsmartins96@hotmail.com>
2022-01-20 20:14:22 -0300

    need to fix: Loop infinito no iOS

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axio from 'axios';
 
 const api = axio.create({
-  baseURL: 'http://10.0.2.2:3333' // iOS: 'http://127.0.0.1:3333'
+  baseURL: 'http://127.0.0.1:3333' // Android: 'http://10.0.2.2:3333'
 });
 
 export default api;
